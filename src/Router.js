8eb91aeb75317ba1e544a85d5d94a9d779559c55
Robert Falkén <f@lken.se>
2020-03-25 16:56:22 +0100

    Start with quiz

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import { Quiz } from "./containers/Quiz";
-import { Feedback } from "./containers/Feedback";
 
 export const Router = () => {
   return (
     <Switch>
-      <Route component={Feedback} />
       <Route component={Quiz} />
     </Switch>
   );
